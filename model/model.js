08441c5d2416c67e58183b146f7dc07b19efe478
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-08-10 01:09:02 +0530

    Add files via upload

---
+++ model/model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  user:{
+    userId:String,
+    profileUrl:String,
+    name:String
+  },
+  post:String,
+  description:{
+    type:String,
+    default:null
+  },
+ likes:{
+   type:Array,
+   default:[]
+ },
+ comments:{
+   type:Array,
+   default:[]
+ },
+  createdAt:{
+    type:Date,
+    default:Date.now()
+  }
+});
+
+const Post =  mongoose.model("post",postSchema);
+export {Post};
